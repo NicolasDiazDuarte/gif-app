3bbe8d2a6cb866cc3d7554a2d6e3f0e2cdf20a1d
NicolasDiazDuarte <niko476678@gmail.com>
2020-07-15 20:36:24 -0300

    separar la funcion getGif y mejora estilos

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -10,7 +10,7 @@ export const AddCategory = ({setCategories}) => {
     const handleSubmit = (e)=> {
         e.preventDefault();
         if (inputValue.trim().length>0){
-            setCategories(cats=>[...cats,inputValue]);
+            setCategories(cats=>[inputValue,...cats]);
             setInputValue('');
         }
         
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,44 +1,31 @@
 import React,{useState,useEffect} from 'react'
 import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid= ({category})  => {
 
     const [images, setImages] = useState([])
 
     useEffect(()=> {
-        getGifs();
-    },[])
+        getGifs(category)
+        .then(setImages);
+    },[category])
 
-    
-   
-    const getGifs = async() => {
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Coraje&limit=10&api_key=gQJ8XFxBCNYuaQLqmFUoTa3f8bDfxkVQ';
-        const resp = await fetch(url);
-        const {data} = await resp.json();
-
-        const gifs = data.map( img =>{
-            return{
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-
-        } )
-
-        setImages(gifs);
-    }
    
     return (
-        <div>
+        <>
             <h3>{category}</h3>
-            {
-                images.map( img=> (
-                    <GifGridItem
-                    key={img.id}  
-                    {...img}
-                    />
-                ))
-            }
-        </div>
+            <div className="card-grid">
+                
+                {
+                    images.map( img=> (
+                        <GifGridItem
+                        key={img.id}  
+                        {...img}
+                        />
+                    ))
+                }
+            </div>
+        </>
     )
 }
