fc3d9b57e10bf15ac723ee80e3018c37bd30e267
NicolasDiazDuarte <niko476678@gmail.com>
2020-07-22 19:00:55 -0300

    tests nuevos agregados

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,9 +3,26 @@ import { shallow } from 'enzyme';
 import {GifGridItem} from '../../components/GifGridItem'
 
 describe('Pruebas en <GifGridItem />', () => {
+   
+   const title = 'un titulo';
+   const url = 'https://localhost/algo.png';
+   const wrapper = shallow ( <GifGridItem title={title} url={url} /> );
+   
    test('Debe mostrar el componente correctamente', () => {
-       const wrapper = shallow ( <GifGridItem/> )
-       expect(wrapper).toMatchSnapshot();
-   })   
-    
+       
+      expect(wrapper).toMatchSnapshot();
+   }) ;  
+   
+   test('debe tener un parrafo con el title ', () => {
+      const p =wrapper.find('p');
+      expect(p.text().trim()).toBe( title );
+   });
+
+   test('debe de tener la imagen igual al url y alt de los props ', () => {
+      const img= wrapper.find('img');
+      expect(img.prop('src')).toBe(url)
+      expect(img.prop('alt')).toBe(title)
+   })
+   
+   
 })
